13856d2b91a1bfd902d7ec5b69e528116edb3f60
bojanangelis <bojanangeleski98@gmail.com>
2023-08-23 22:07:47 +0200

    done

--- main.c
+++ main.c
@@ -1,7 +1,51 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void cleanString(char *text) {
+    text[strlen(text) - 1] = '\0';
+}
+
+int checkCondition(char *str) {
+    // proverka dali e palindrom
+    int i, isPalindrome = 1, hasSpecialChar = 0;
+    int n = strlen(str);
+    for (i = 0; i < strlen(str) / 2; i++) {
+        if (str[i] != str[n - i - 1]) {
+            isPalindrome = 0;
+        }
+    }
+    for (i = 0; i < strlen(str) / 2; i++) {
+        if (!isalnum(str[i])) {
+            hasSpecialChar = 1;
+        }
+    }
+    return isPalindrome == 1 && hasSpecialChar == 1;
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int i, N;
+    scanf("%d\n", &N);
+    int maxLen = 0;
+    char max[81];
+
+    for (i = 0; i < N; i++) {
+        char line[81];
+        fgets(line, sizeof(line), stdin);
+        cleanString(line);
+        if (checkCondition(line)) {
+            if (strlen(line) > maxLen) {
+                maxLen = strlen(line);
+                strcpy(max, line);
+            }
+        }
+    }
+    if (maxLen != 0) {
+        puts(max);
+    } else {
+        printf("Nema!");
+    }
+
     return 0;
 }
 
